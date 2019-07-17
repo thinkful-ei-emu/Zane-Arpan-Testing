950e99ec9d72ac94ccd00cb75e95e013ac0a1d67
Zane Kuehn <zanekuehn@yahoo.com>
2019-07-17 11:45:17 -0500

    In the middle of testing GET /frequency

--- express_testing/test/app.test.js
+++ express_testing/test/app.test.js
@@ -2,28 +2,47 @@ const app = require('../app');
 const expect = require('chai').expect;
 const request = require('supertest');
 
-describe('Express App', () => {
-  it('GET / should return a message', () => {
-    return request(app)
-      .get('/')
-      .expect(200, 'Hello Express!');
+// describe('Express App', () => {
+//   it('GET / should return a message', () => {
+//     return request(app)
+//       .get('/')
+//       .expect(200, 'Hello Express!');
+//   });
+// });
+
+// describe('GET /sum', ()=>{
+
+//   it('8/4 = 2', ()=>{
+
+//     return request(app)
+//       .get('/sum')
+//       .query({a:8, b:4})
+//       .expect(200, '8 divided by 4 is 2');
+//   });
+
+//   it('should return 400 if a is missing', ()=>{
+//     return request(app)
+//       .get('/sum')
+//       .query({b:4})
+//       .expect(400, 'Value for a is needed');
+//   });
+
+
+// });
+
+describe('GET/frequency', () => {
+  it('Object Returns as expected', () => {
+    return request(app).get('/frequency')
+      .query({ s: 'aaBBAAbbaa' }).expect(200, {
+        'count': 2,
+        'average': 5,
+        'highest': 'a',
+        'a': 6,
+        'b': 4
+      });
   });
-});
-
-describe('GET /sum', ()=>{
-  
-  it('8/4 = 2', ()=>{
-
-    return request(app)
-    .get('/sum')
-    .query({a:8, b:4})
-    .expect(200, '8 divided by 4 is 2');
+  it('Should get Invalid Request if s is undefined ',()=>{
+    return request(app).get('/frequency').query({})
+      .expect(400,'Invalid request');
   });
-
-  it('should return 400 if a is missing', ()=>{
-    return request(app)
-    .get('/sum')
-    .query({b:4})
-    .expect(400, 'Value for a is needed');
-  })
 });
